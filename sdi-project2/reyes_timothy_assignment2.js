f040e6fa2dfafdc7388f75411ae04ce8eb1ac922
Timothy Reyes <timothyreyes@Timothys-MacBook-Pro.local>
2013-01-14 12:44:53 -0800

    Updated js file

--- sdi-project2/reyes_timothy_assignment2.js
+++ sdi-project2/reyes_timothy_assignment2.js
@@ -1,1 +1,35 @@
-alert("JavaScript works!");
+// Timothy Reyes
+// SDI 1301
+// Assignment: Project 2
+
+
+
+var boredOfJob = true
+var jobDuties = ["Online Support Chat", "Phone Support", "Email Support"]
+
+//Procedure
+
+
+var storyIntro = function(name, yearsWorked, boredOfJob) {
+
+console.log(name + " has worked at Compix for the past " + yearsWorked + ", he is starting to look outside his bubble and embark on a new journey into Mobile Development.");
+
+if (boredOfJob == true){
+console.log("I'm starting to get bored of my job, and here is an example why.")
+} //if statement 
+	else {
+		console.log("I currently love my job, and I don't want to embark on Mobile Development at this time.");	
+	}; //else statment
+
+}; //storyIntro function
+storyIntro("Timothy Reyes", "10 years", boredOfJob);
+
+
+
+//Boolean Function
+
+var performDailyTasks = function () {
+	
+	
+}; // performDailyTasks function
+performDailyTasks()
