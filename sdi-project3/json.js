4c12e6f1e672327bd38160f3c675cbf070a503fd
Timothy Reyes <timothyreyes@Timothys-MacBook-Pro.local>
2013-01-23 08:34:12 -0800

    I've started coding on the 2x javascript files.  I noticed after the gotoTraining that I'm not suppose to have functions in my json file, so I'll have to edit them out today.  Still have a lot of work to do.

--- sdi-project3/json.js
+++ sdi-project3/json.js
@@ -1,52 +1,64 @@
 // This is a JSON File
 // JSON's just use JavaScripts Objects and Arrays
 
-var json = {
-	"friends": {
-		"09888890": {
-			"name": "Chris",
-			"age": 23
-		},
-		"09888891": {
-			"name": "Scott",
-			"age": 43
-		},
-		"09888892": {
-			"name": "Lisa",
-			"age": 20
-		}
-	}
-	
-}; // json object
-
-
-json.friends["09888890"].name
-
-
-for (var key in json.friends) {
-	var friend = json.friends[key];
-	// friend.name or .age
-};
-
-
-
-var json2 = {
-	"friends": [
-		{
-			"userId" : "09888890",
-			"name" : "Chris",
-			"age" : 23
-		},
-		{
-					"userId" : "09888891",
-			"name" : "Scott",
-			"age" : 43
-		},
-		{
-					"userId" : "09888892",
-			"name" : "Lisa",
-			"age" : 20
-		}
-	]
-	
-};
+var family = {// value object
+		"children" : [
+			{"name" : "Oliver",
+			"wakesUp": 7.5, // property:Number
+			"favoriteCereals": [ //property: Array
+				"Cheerios",
+				"Mini-wheats",
+				"Apple Jacks"
+			],
+			"favoriteCloths": [ //property Array
+				"Tee-shirt and jeans",
+				"Long-sleeve shirt and jeans",
+				"Thomas the Train shirt and shorts"
+			],
+			"brushedTeeth": true, //property Boolean
+			"gotChanged": true, //property Boolean
+			"ateBreakfast": true, //property Boolean
+			"readyForSchool" : readyForSchool = function (Teeth, Changed, Breakfast){
+				if (Teeth && Changed && Breakfast === true) {
+					console.log("We are ready to go dad!");
+				}
+				else (console.log("Not ready yet..."))
+			}// readyForSchool function
+			}, // children 0
+			
+			{"name" : "Willow",
+			"wakesUp": 7.5, // property:Number
+			"favoriteCereals": [ //property: Array
+				"Chocolate Cheerios",
+				"Fruity Pebbles",
+				"Apple Jacks"
+			],
+			"favoriteCloths": [ //property Array
+				"Tee-shirt and jeans",
+				"Long-sleeve shirt and jeans",
+				"Thomas the Train shirt and shorts"
+			],
+			"brushedTeeth": true, //property Boolean
+			"gotChanged": true, //property Boolean
+			"ateBreakfast": true, //property Boolean
+			"readyForSchool" : readyForSchool = function (Teeth, Changed, Breakfast){
+				if (Teeth && Changed && Breakfast === true) {
+					console.log("We are ready to go dad!");
+				}
+					else (console.log("Not ready yet dad, give me another minute!"))
+			}// readyForSchool function
+			} //children 1
+		],// children array
+		
+		"parents" : [
+			{"name" : "Timothy",
+			"wakesUp" : 7, // property:Number
+			"readyToGo" : readyToGo = function (readyChild0){
+				if (readyChild0 === true && readyChild1 === true) {
+					console.log("Okay children, let's go to School!");
+					}
+					else (console.log("We can't leave until you finish!"))
+				}//readytogo function
+			}//Timothy
+		]// parents array
+}; //family object
--- sdi-project3/reyes_timothy_assignment3.js
+++ sdi-project3/reyes_timothy_assignment3.js
@@ -1,1 +1,67 @@
-alert("JavaScript works!");
+//opening
+/* STRING FUNCTION
+concatenation = function (current, neww) {
+
+	var outputStatement = "After graduating with my Mobile Development Degree, I will be going from working on " + current + " to working in " + neww + ".  Exciting times!"
+	
+	return outputStatement
+	
+}; // concatenation function
+
+var statement = concatenation(currentJobDuties, newJobDuties)
+
+output(statement);
+*/
+var parent0 = family.parents[0].name,
+	child0 = family.children[0].name,
+	child1 = family.children[1].name;
+//String Function
+var beginningSentence = function (parent, child1, child2) {		
+	var sentence = parent + "'s having trouble sleeping.  He is constantly looking at his clock throughout the night waiting to begin a new day.  As soon as he gets up he'll need to get " + child1 + " and " + child2 + " ready for school."
+	return sentence
+	
+}// beginningSentence function
+
+var statement = beginningSentence(parent0, child0, child1)
+console.log(statement);
+
+
+//beginning numbers: number argument, property number, while loop, return number, math
+checkingClock = function (timeToLeave){
+	
+	var clock = 1;
+	
+	while(clock < family.parents[0].wakesUp) {
+	console.log("It's " + clock + "AM, not quite " + family.parents[0].wakesUp + "AM yet; I'll go back to sleep.");
+	clock++;
+}; //while loop
+ 
+return clock
+
+}; // checkingClock function
+
+console.log("Finally it's " + checkingClock() + "AM, time to get the kids ready for school!");
+
+// end numbers section
+
+
+console.log(family.parents[0].name + "'s children names are " + family.children[0].name + " and " + family.children[1].name + ".");
+
+var handleData = function(family) {
+//	console.log(family);
+	for (var i = 0; i < family.children.length; i++){
+		var child = family.children[i];
+		console.log("Hello " + child.name + "!");
+		
+	}; //for loop
+	
+};
+handleData(family);
+
+console.log("Let's get " + family.parents[0].name + "'s children ready for school this morning.");
+
+var readyChild0 = 			family.children[0].readyForSchool(family.children[0].brushedTeeth,family.children[0].gotChanged, family.children[0].ateBreakfast),	
+	readyChild1 = family.children[1].readyForSchool(family.children[1].brushedTeeth,family.children[1].gotChanged, family.children[1].ateBreakfast)
+
+
+console.log(readyChild0);
