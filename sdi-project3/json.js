07a0da18423de58da645df85d6399b271a7bb90b
Timothy Reyes <timothyreyes@Timothys-MacBook-Pro.local>
2013-01-23 20:14:48 -0800

    I've heavily updated the javascript AND json file.  It is finally taking shape.  Originally I had major functions inside my json file, had to tear them out and place them accordingly, which was a big task.

--- sdi-project3/json.js
+++ sdi-project3/json.js
@@ -1,47 +1,41 @@
 // This is a JSON File
 // JSON's just use JavaScripts Objects and Arrays
 
-var family = {// value object
-		"children" : [
-			{"name" : "Oliver",
-			"wakesUp": 7.5, // property:Number
-			"favoriteCereals": [ //property: Array
-				"Cheerios",
-				"Mini-wheats",
-				"Apple Jacks"
+var morningRoutine = {
+		favoriteCereals : {
+			bad : [
+			"Fruity Pebbles", 
+			"Chocolate Cheerios", 
+			"Cinniman Toast Crunch"
 			],
-			"favoriteCloths": [ //property Array
-				"Tee-shirt and jeans",
-				"Long-sleeve shirt and jeans",
-				"Thomas the Train shirt and shorts"
+			good : [
+			"Cheerios", 
+			"Wheaties", 
+			"Oatmeal"
+			]
+		},
+		favoriteCloths : {
+			boy : [
+			"tee-shirt and jeans", 
+			"Thomas the Train shirt and shorts", 
+			"long-sleeve shirt and jeans"
 			],
-			"brushedTeeth": false, //property Boolean
-			"gotChanged": true, //property Boolean
-			"ateBreakfast": true //property Boolean
-
-			}, // children 0
-			
-			{"name" : "Willow",
-			"wakesUp": 7.5, // property:Number
-			"favoriteCereals": [ //property: Array
-				"Chocolate Cheerios",
-				"Fruity Pebbles",
-				"Apple Jacks"
-			],
-			"favoriteCloths": [ //property Array
-				"Tee-shirt and jeans",
-				"Long-sleeve shirt and jeans",
-				"Thomas the Train shirt and shorts"
-			],
-			"brushedTeeth": true, //property Boolean
-			"gotChanged": true, //property Boolean
-			"ateBreakfast": true //property Boolean
-						} //children 1
-		],// children array
-		
-		"parents" : [
-			{"name" : "Timothy",
-			"wakesUp" : 7 // property:Number
-			}//Timothy
-		]// parents array
-}; //family object
+			girl : [
+			"purple dress", 
+			"red blouse", 
+			"tee-shirt and shorts"
+			]
+		},
+		childrenTasks : {
+			oliverTask : {
+				"brushedTeeth" : false,
+				"gotChanged": true,
+				"ateBreakfast": true
+				},
+			willowTask : {
+				"brushedTeeth": false,
+				"gotChanged": true,
+				"ateBreakfast": true
+				}
+		}//childrenTasks Object
+}; //morning Routine Object
--- sdi-project3/reyes_timothy_assignment3.js
+++ sdi-project3/reyes_timothy_assignment3.js
@@ -12,61 +12,94 @@ var statement = concatenation(currentJobDuties, newJobDuties)
 
 output(statement);
 */
-var childOliver = family.children[0],
-	childWillow = family.children[1],
-	parentTimothy = family.parents[0]
+
+
+//ACCESSOR METHOD
+var myFamily = {
+	children : ["Oliver", "Willow"],
+	parents : ["Timothy", "Veronica"],
+	needsMoreSleep: "I'll go back to sleep!",
+	familyCereal : {
+		good : ["Cheerios, Wheaties, Oatmeal"],
+		bad : ["Fruity Pebbles", "Chocolate Cheerios", "Cinniman Toast Crunch"]
+	},// familyCereal Object
+	beginningSentence : function (parent, child1, child2) {
+			var sentence = parent + "'s having trouble sleeping.  He is constantly looking at his clock throughout the night waiting to begin a new day.  As soon as he gets up he'll need to get " + child1 + " and " + child2 + " ready for school."
+			
+		return sentence
+		
+	}// beginningSentence function	
+};// family object
+console.log(myFamily.beginningSentence(myFamily.parents[0], myFamily.children[0], myFamily.children[1]));
+
+// END ACCESSOR METHOD
 
 
 
-//String Function
-
-var beginningSentence = function (parent, child1, child2) {		
-	var sentence = parent + "'s having trouble sleeping.  He is constantly looking at his clock throughout the night waiting to begin a new day.  As soon as he gets up he'll need to get " + child1 + " and " + child2 + " ready for school."
-	return sentence
-	
-}// beginningSentence function
-
-var statement = beginningSentence(parentTimothy.name, childOliver.name, childWillow.name)
-console.log(statement);
-
-
-//beginning numbers: number argument, property number, while loop, return number, math
-var mainClock = checkingClock = function (timeToLeave){
+//FUNCTION METHOD
+var time = { 
+	timothyWakesUp : 7,
+	checkingClock : function (timothyWakesUp, timothyNeedsSleep){
 	
 	var clock = 1;
 	
-	while(clock < parentTimothy.wakesUp) {
-	console.log("It's " + clock + "AM, not quite " + parentTimothy.wakesUp + "AM yet; I'll go back to sleep.");
+	while(clock < time.timothyWakesUp) {
+	console.log("It's " + clock + "AM, not quite " + time.timothyWakesUp + "AM yet; " + timothyNeedsSleep);
 	clock++;
 }; //while loop
  
 return clock
 
-}; // checkingClock function
+} // checkingClock function
+} // time object
 
-console.log("Finally it's " + checkingClock() + "AM, time to get the kids ready for school!");
+console.log("Finally it's " + time.checkingClock(time.timothyWakesUp, myFamily.needsMoreSleep) + "AM, time to get the kids ready for school!");
 
-// end numbers section
+// END FUNCTION METHOD
 
+console.log(myFamily.parents[0] + "s and " + myFamily.parents[1] + "s children names are " + myFamily.children[0] + " and " + myFamily.children[1] + ".");
 
-console.log(parentTimothy.name + "'s children names are " + childOliver.name + " and " + childWillow.name + ".");
+//PROCEDURE METHOD
 
+var	handleData = function(myFamily) { // for loop , proedure method
 
-var handleData = function(family) { // for loop , proedure method
-//	console.log(family);
-	for (var i = 0; i < family.children.length; i++){
-		var child = family.children[i];
-		console.log("Hello " + child.name + "!");
-		
+	for (var i = 0; i < myFamily.children.length; i++){
+		var child = myFamily.children[i];
+		console.log("Hello " + child + "!");
+		}
 	}; //for loop
 	
-};
-handleData(family);
+handleData(myFamily);
+
+//END PROCEDURE METHOD
+
+//MUTATOR METHOD
+
+var eatAndChange = {
+		availableCereal : ["Cheerios", "Kellogs Frosted Flakes"],
+		oliverFavoriteCereal : morningRoutine.favoriteCereals.bad[2],
+		oliverFavoriteCloths : 1,
+		oliverPickCloths : function (oliverFavoriteCloths){
+			while(oliverFavoriteCloths < morningRoutine.favoriteCloths.boy.length){
+	console.log("It's ");
+	oliverFavoriteCloths++;
+		}//while loop
+		};//function
+eatAndChange.oliverPickCloths(oliverFavoriteCloths);
+		//willowPickCloths : function (){
+			
+		//}
+		//willowFavoriteCereal : morningRoutine.favoriteCereals.bad[0],
+		
+		//pickCereal : function (){
+			
+			
+			
+		//}
 
 
-
-// booliean : method function
-console.log("Let's get " + family.parents[0].name + "'s children ready for school this morning.");
+// boolean : method function
+/*console.log("Let's get " + family.parents[0].name + "'s children ready for school this morning.");
 
 var OliverReadyForSchool = function (brushTeeth, getChanged, haveBreakfast){
 				if (brushTeeth && getChanged && haveBreakfast === true) {
@@ -91,4 +124,4 @@ var readyToGo = function (readyOliver, readyWillow){
 					else (console.log("We can't leave until you finish!"))
 
 }
-var readyToGo = readyToGo(OliverReadyForSchool(childOliver.brushedTeeth,childOliver.gotChanged, childOliver.ateBreakfast) === true, WillowReadyForSchool(childWillow.brushedTeeth,childWillow.gotChanged, childWillow.ateBreakfast))
+var readyToGo = readyToGo(OliverReadyForSchool(childOliver.brushedTeeth,childOliver.gotChanged, childOliver.ateBreakfast) === true, WillowReadyForSchool(childWillow.brushedTeeth,childWillow.gotChanged, childWillow.ateBreakfast))*/
