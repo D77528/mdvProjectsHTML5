60c5dd3440f336b2b36b0664eca44402258f035b
Timothy Reyes <htiml5@fullsail.edu>
2013-02-21 13:25:59 -0800

    I've completed assignment 3.  After 3 days of searching I found out why my Tickets weren't updating, rather adding onto the Display.  Did not have window.location.reload() within the showData function.

--- vf_project3/js/main.js
+++ vf_project3/js/main.js
@@ -1,18 +1,17 @@
 //Reyes, Timothy
 //Virtual Frameworks 1302
-//Project Web App Part 2
-//GitHub: https://github.com/D77528/mdvProjectsHTML5/tree/gh-pages/vf_project2
+//Project Web App Part 3
+//GitHub: https://github.com/D77528/mdvProjectsHTML5/tree/gh-pages/vf_project3
 
 
 window.addEventListener("DOMContentLoaded", function(){
 
-//Global variables
 
+//Global variables
 	var supportTypeList = ["--> Choose Support Type", "Call", "Email", "Online Chat", "Support Form"];
 	var responseBox = "No";
 	var endUserValue;
-	var makeSelect;
-	var errorM = element("anyErrors")
+	var errorM = element("anyErrors");
 	
 	function element(x){
 		var ElementGrabber = document.getElementById(x);
@@ -21,7 +20,6 @@ window.addEventListener("DOMContentLoaded", function(){
 	
 	
 //Dropdown List: supportType
-	
 	function dynamicInsert() {
 		var formTag = document.getElementsByTagName("form"),
 			selectListItem = element("select"),
@@ -38,12 +36,12 @@ window.addEventListener("DOMContentLoaded", function(){
 		}
 		selectListItem.appendChild(makeSelect);
 		
-	};
+	}
 	
 	dynamicInsert()
 
-//Radio Buttons for Contact Type
 
+//Radio Buttons for Contact Type
 	function getEndUserValue(){
 		var contactRadios = document.forms[0].contactType;
 		for(var i=0; i<contactRadios.length; i++){
@@ -55,8 +53,8 @@ window.addEventListener("DOMContentLoaded", function(){
 		
 	};
 
-//Checkbox for Waiting for Response
 
+//Checkbox for Waiting for Response
 	function getCheckValue(){
 		if(element("awaitingResponse").checked){
 			responseBox = element("awaitingResponse").value;
@@ -68,7 +66,6 @@ window.addEventListener("DOMContentLoaded", function(){
 	
 
 //ToggleContent for Display
-
 	function toggleContent(v){
 		switch(v){
 			case "on":
@@ -98,10 +95,12 @@ window.addEventListener("DOMContentLoaded", function(){
 	
 
 //Submit Button pressed
-
-	function showData(){
+	function showData(key){
+	if(!key){
 		var id	= Math.floor(Math.random()*293842);
-		
+		}else{
+		id = key;
+		}
 		getEndUserValue();
 		getCheckValue();
 		
@@ -118,16 +117,19 @@ window.addEventListener("DOMContentLoaded", function(){
 				response : ["Waiting for Response: ", responseBox],
 				comments : ["Support Overview: ", element("comments").value]
 			}
-			alert ("Your Ticket has been submitted.")
+			alert ("Ticket has been submitted")
 			localStorage.setItem(id, JSON.stringify(dataItems));
-		
+			window.location.reload()
 	};
 	
 
-//Display Link is pressed	
 
+//Display Link is pressed	
 	function displayD(){
 		toggleContent("on");
+		if(localStorage.length === 0){
+			alert("There are no Support Tickets submitted.")
+		}
 			var HTMLDiv = document.createElement("div");
 			HTMLDiv.setAttribute("id", "dataItems", "align");
 			HTMLDiv.align ="left";
@@ -136,56 +138,58 @@ window.addEventListener("DOMContentLoaded", function(){
 			document.body.appendChild(HTMLDiv);
 			element("dataItems").style.display = "block";
 			element("dataItems").style.display = ""
-			for(var i=0, localStorageLen=localStorage.length; i<localStorageLen; i++){
+			for(var i=0, j=localStorage.length; i<j; i++){
 				var li = document.createElement("li");
-				var linksLi = document.createElement("li");//edit
+				var createLi = document.createElement("li");//edit
 				newList.appendChild(li);
-				var code = localStorage.key(i);
-				var value = localStorage.getItem(code);
+				var key = localStorage.key(i);
+				var value = localStorage.getItem(key);
 				var object = JSON.parse(value);
 				var makeTabList = document.createElement("ul");
 				makeTabList.style.border="2px solid black";
 				makeTabList.style.padding="10px";
 				makeTabList.width-"200px";
-				var inc	= Math.floor(Math.random()*1000001);
-				makeTabList.innerHTML = "Ticket Number: " + inc;
+				//var inc	= Math.floor(Math.random()*1000001);
+				//makeTabList.innerHTML = "Ticket Number: " + inc;
 				li.appendChild(makeTabList);
 				for(var v in object){
 					var makeTabli = document.createElement("li");
 					makeTabList.appendChild(makeTabli);
 					var text = object[v][0] + object[v][1];
 					makeTabli.innerHTML = text;
-					makeTabList.appendChild(linksLi); //edit linksLi
+					makeTabList.appendChild(createLi);
 				}
-			makeItemLinks(localStorage.key(i), linksLi); //create edit and delete buttons for each item in local storage	
-				
+			makeItemLinks(localStorage.key(i), createLi);	
 		}	
-	}
-//console.log(localStorage.key);
-	
-	function makeItemLinks(code, linksLi){ //function edit LinksLi is being used in above funciton from week2
-		var editLink = document.createElement("a");
-		editLink.href = "#";
-		editLink.code = code;
+	};
+
+
+// Links for Edit & Delete Ticket	
+	function makeItemLinks(key, createLi){
+		var createEditLink = document.createElement("a");
+		createEditLink.href = "#";
+		createEditLink.key = key;
 		var editText = "Edit Ticket";
-		editLink.addEventListener("click", editItem);
-		editLink.innerHTML = editText;
-		linksLi.appendChild(editLink);
+		createEditLink.addEventListener("click", editTicket);
+		createEditLink.innerHTML = editText;
+		createLi.appendChild(createEditLink);
 		
 		var breakTag = document.createElement("br");
-		linksLi.appendChild(breakTag);
+		createLi.appendChild(breakTag);
 		
-		var deleteLink = document.createElement("a");
-		deleteLink.href = "#";
-		deleteLink.code = code;
+		var createDeleteLink = document.createElement("a");
+		createDeleteLink.href = "#";
+		createDeleteLink.key = key;
 		var deleteText = "Delete Ticket";
-		//deleteLink.addEventListener("click", deleteItem);
-		deleteLink.innerHTML = deleteText;
-		linksLi.appendChild(deleteLink);
-	}
-	
-	function editItem(){//edit please
-		var value = localStorage.getItem(this.code);
+		createDeleteLink.addEventListener("click", deleteTicket);
+		createDeleteLink.innerHTML = deleteText;
+		createLi.appendChild(createDeleteLink);
+	};
+
+
+//Edit Link Displays
+	function editTicket(){
+		var value = localStorage.getItem(this.key);
 		var item = JSON.parse(value);
 		
 		toggleContent("off");
@@ -220,111 +224,119 @@ window.addEventListener("DOMContentLoaded", function(){
 		element("button").value = "Edit Ticket";
 		var editButton = element("button");
 		editButton.addEventListener("click", validate);
-		editButton.key = this.code;
+		editButton.key = this.key;
 		
-	}
+	};
 
 
-
-//Clear button
-
+//Delete Link Displays
+	function deleteTicket(){
+		var conf = confirm("Are you sure you want to delete this Ticket?");
+		if(conf){
+			localStorage.removeItem(this.key);
+			alert("Ticket deleted");
+			window.location.reload()
+			
+		}else{
+			alert("Cancelled");
+		}
+	};
+	
+	
+	
+//Clear Ticket Link
 	function clearLocal(){
 		if(localStorage.length === 0){
-			alert("Data is not present.")
+			alert("No Support Tickets found")
 			
 		}else{
 			localStorage.clear();
-			alert("All Support Tickets are deleted.");
+			alert("All Support Tickets deleted");
 			window.location.reload();
 		}
 		
 	};
-
-function validate(evt){
-	var getSupportType = element("supportType");
-	var getFirstn = element("firstn");
-	var getLastn = element("lastn");
-	var getEmail = element("email");
-	var getComments = element("comments");
 	
-	errorM.innerHTML="";
-		getSupportType.style.border="1px solid black";
-		getFirstn.style.border="1px solid black";
-		getLastn.style.border="1px solid black";
-		getEmail.style.border="1px solid black";
 	
-	var validations = [];
+//Validation Function
+	function validate(e){
+		var getSupportType = element("supportType");
+		var getFirstn = element("firstn");
+		var getLastn = element("lastn");
+		var getEmail = element("email");
+		var getComments = element("comments");
+		
+		errorM.innerHTML="";
+			getSupportType.style.border="1px solid black";
+			getFirstn.style.border="1px solid black";
+			getLastn.style.border="1px solid black";
+			getEmail.style.border="1px solid black";
+			getComments.style.border="1px solid black";
+		
+		var allErrors = [];
 	
-	if(getSupportType.value === "--> Choose Support Type"){
-		var getSupportTypeError = "Select a Support Type";
-		getSupportType.style.border="2px dotted red";
-		validations.push(getSupportTypeError);
 		
-	}
-	if(getFirstn.value === ""){
-	var firstnError = "First Name Required";
-	getFirstn.style.border="2px dotted red";
-	validations.push(firstnError);
-		
-		
-	}
-	if(getLastn.value === ""){
-	var lastnError = "Last Name Required";
-	getLastn.style.border="2px dotted red";
-	validations.push(lastnError);
-		
-		
-	}
-	if(getEmail.value === ""){
-	var regularEx = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-	if(!(regularEx.exec(getEmail.value))){
-		var eError = "Valid Email Required";
-		getFirstn.style.border="2px dotted red";
-		validations.push(eError)
-		
-	}
-
-	
-	var emailError = "Email Required";
-	getEmail.style.border="2px dotted red";
-	validations.push(emailError);
-		
-		
-	}
-	if(getComments.value === ""){
-		var commentsError = "Support Overview Required";
-		getComments.style.border="2px dotted red";
-		validations.push(commentsError);
-		
-		
-	}
-	if (validations.length >= 1){
-		for(var i=0, j=validations.length; i<j; i++){
-			var text = document.createElement("li");
-			text.style.color="red";
-			text.innerHTML = validations[i];
-			errorM.appendChild(text);
+		if(getSupportType.value === "--> Choose Support Type"){
+			var getSupportTypeError = "Support Type Required";
+			getSupportType.style.border="2px dotted red";
+			allErrors.push(getSupportTypeError);
+			
 		}
-	evt.preventDefault();
-	return false;
+		if(getFirstn.value === ""){
+			var firstnError = "First Name Required";
+			getFirstn.style.border="2px dotted red";
+			allErrors.push(firstnError);
+			
+			
+		}
+		if(getLastn.value === ""){
+			var lastnError = "Last Name Required";
+			getLastn.style.border="2px dotted red";
+			allErrors.push(lastnError);
+			
+			
+		}
+		var regularEx = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+		if(!(regularEx.exec(getEmail.value))){
+			var eError = "Valid Email Required";
+			getEmail.style.border="2px dotted red";
+			allErrors.push(eError)
+			
+		}
 	
+		if(getComments.value === ""){
+			var commentsError = "Support Overview Required";
+			getComments.style.border="2px dotted red";
+			allErrors.push(commentsError);
+			
+			
+		}
+		if (allErrors.length >= 1){
+			for(var i=0, j=allErrors.length; i<j; i++){
+				var textAtt = document.createElement("li");
+				textAtt.style.color="red";
+				textAtt.innerHTML = allErrors[i];
+				errorM.appendChild(textAtt);
+			}
+			e.preventDefault();
+			return false;
+			
+		}else{
+			showData(this.key)
+		}
 		
-	}else{
-		showData()
-	}
 	
+	};
 
-}
 
-	//Links: displayData, clearData & Submit Button
-	
+//Links: displayData, clearData & Submit Button	
 	var displayData = element("displayData");
 	displayData.addEventListener("click", displayD);
 
 	var clearData = element("clearStoredData");
 	clearData.addEventListener("click", clearLocal);
 
-	var submit = element("button");
+	var button = element("button");
 	button.addEventListener("click", validate);
 	
 
