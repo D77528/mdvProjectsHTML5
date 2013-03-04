38fc1a76b7058f123754b36dad665cceb6afe658
Timothy Reyes <htiml5@fullsail.edu>
2013-03-04 09:33:44 -0800

    Here is my Visual Frameworks project

---
+++ miu/week1/js/json.js
@@ -0,0 +1,39 @@
+var jsonObjects = {
+	"ticket1": {
+		"firstn" : ["First Name: ", "Timothy"],
+		"lastn" : ["Last Name: ", "Reyes"],
+		"email" : ["Email Address: ", "tim.reyes@compix.tv"],
+		"phone" : ["Phone Number: ", "(949)585-0055"],
+		"date" : ["Date Entered: ", "1978-12-15"],
+		"contactType" : ["Contact Type: ", "End User"],
+		"contactFeelings" : ["Contact Feelings Happy: 0 ~ Upset: 100: ", "75"],
+		"supportType" : ["Support Type: ", "Call"],
+		"response" : ["Waiting for Response: ", "Yes"],
+		"comments" : ["Support Overview: ", "There is nothing to see here."]
+	},
+	"ticket2": {
+		"firstn" : ["First Name: ", "Veronica"],
+		"lastn" : ["Last Name: ", "Kim"],
+		"email" : ["Email Address: ", "veronica.kim@compix.tv"],
+		"phone" : ["Phone Number: ", "(949)585-0055"],
+		"date" : ["Date Entered: ", "1983-01-28"],
+		"contactType" : ["Contact Type: ", "Dealer"],
+		"contactFeelings" : ["Contact Feelings Happy: 0 ~ Upset: 100: ", "25"],
+		"supportType" : ["Support Type: ", "Email"],
+		"response" : ["Waiting for Response: ", "No"],
+		"comments" : ["Support Overview: ", "There is nothing to see here."]
+	},
+	"ticket3": {
+		"firstn" : ["First Name: ", "Oliver"],
+		"lastn" : ["Last Name: ", "Reyes"],
+		"email" : ["Email Address: ", "oliver.reyes@compix.tv"],
+		"phone" : ["Phone Number: ", "(949)585-0055"],
+		"date" : ["Date Entered: ", "2010-04-23"],
+		"contactType" : ["Contact Type: ", "End User"],
+		"contactFeelings" : ["Contact Feelings Happy: 0 ~ Upset: 100: ", "100"],
+		"supportType" : ["Support Type: ", "Online_Chat"],
+		"response" : ["Waiting for Response: ", "Yes"],
+		"comments" : ["Support Overview: ", "There is nothing to see here."]
+	}
+	
+}
